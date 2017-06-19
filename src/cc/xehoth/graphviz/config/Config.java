48f8ceb1f0c435359dbe33d9d99dc6d980744e14
xehoth <824547322@qq.com>
2017-06-19 15:00:18 +0800

    Add Data Saving v1.03

--- src/cc/xehoth/graphviz/config/Config.java
+++ src/cc/xehoth/graphviz/config/Config.java
@@ -12,98 +12,98 @@ import java.io.Serializable;
 
 public class Config implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
-	private boolean isAutoPainting = false;
+    private boolean isAutoPainting = false;
 
-	private String content = null;
+    private String content = null;
 
-	private boolean isDirected = false;
+    private boolean isDirected = false;
 
-	private boolean hasWeight = false;
+    private boolean hasWeight = false;
 
-	private boolean saveLastData = true;
+    private boolean saveLastData = true;
 
-	public boolean isAutoPainting() {
-		return isAutoPainting;
-	}
+    public boolean isAutoPainting() {
+        return isAutoPainting;
+    }
 
-	public void setAutoPainting(boolean isAutoPainting) {
-		this.isAutoPainting = isAutoPainting;
-	}
+    public void setAutoPainting(boolean isAutoPainting) {
+        this.isAutoPainting = isAutoPainting;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	public boolean isDirected() {
-		return isDirected;
-	}
+    public boolean isDirected() {
+        return isDirected;
+    }
 
-	public void setDirected(boolean isDirected) {
-		this.isDirected = isDirected;
-	}
+    public void setDirected(boolean isDirected) {
+        this.isDirected = isDirected;
+    }
 
-	public boolean isHasWeight() {
-		return hasWeight;
-	}
+    public boolean isHasWeight() {
+        return hasWeight;
+    }
 
-	public void setHasWeight(boolean hasWeight) {
-		this.hasWeight = hasWeight;
-	}
+    public void setHasWeight(boolean hasWeight) {
+        this.hasWeight = hasWeight;
+    }
 
-	public boolean isSaveLastData() {
-		return saveLastData;
-	}
+    public boolean isSaveLastData() {
+        return saveLastData;
+    }
 
-	public void setSaveLastData(boolean saveLastData) {
-		this.saveLastData = saveLastData;
-	}
+    public void setSaveLastData(boolean saveLastData) {
+        this.saveLastData = saveLastData;
+    }
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
 
-	public static Config getData() throws FileNotFoundException, IOException, ClassNotFoundException {
-		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("data.dat"));
-		Config config = (Config) ois.readObject();
-		ois.close();
-		ois = null;
-		BufferedReader br = new BufferedReader(new FileReader("config"));
-		String s = br.readLine();
-		if (s.trim().endsWith("true")) {
-			config.isAutoPainting = true;
-		} else {
-			config.isAutoPainting = false;
-		}
-		s = br.readLine();
-		if (s.trim().endsWith("true")) {
-			config.saveLastData = true;
-		} else {
-			config.saveLastData = false;
-		}
-		br.close();
-		br = null;
-		return config;
-	}
+    public static Config getData() throws FileNotFoundException, IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("data.dat"));
+        Config config = (Config) ois.readObject();
+        ois.close();
+        ois = null;
+        BufferedReader br = new BufferedReader(new FileReader("config"));
+        String s = br.readLine();
+        if (s.trim().endsWith("true")) {
+            config.isAutoPainting = true;
+        } else {
+            config.isAutoPainting = false;
+        }
+        s = br.readLine();
+        if (s.trim().endsWith("true")) {
+            config.saveLastData = true;
+        } else {
+            config.saveLastData = false;
+        }
+        br.close();
+        br = null;
+        return config;
+    }
 
-	public static void saveData(Config config) throws FileNotFoundException, IOException {
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("data.dat"));
-		oos.writeObject(config);
-		oos.flush();
-		oos.close();
-		oos = null;
-	}
+    public static void saveData(Config config) throws FileNotFoundException, IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("data.dat"));
+        oos.writeObject(config);
+        oos.flush();
+        oos.close();
+        oos = null;
+    }
 
-	/*
-	 * public static void main(String[] args) { try { saveData(new Config()); }
-	 * catch (IOException e) { e.printStackTrace(); } }
-	 */
+    /*
+     * public static void main(String[] args) { try { saveData(new Config()); }
+     * catch (IOException e) { e.printStackTrace(); } }
+     */
 }
